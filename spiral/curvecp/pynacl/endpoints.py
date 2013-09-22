760e196e425323987deb45b4a76d3891770fdfdd
Aaron Gallagher <habnabit@gmail.com>
2013-09-22 10:41:59 -0700

    Remove more debugging code.

--- spiral/curvecp/pynacl/endpoints.py
+++ spiral/curvecp/pynacl/endpoints.py
@@ -23,7 +23,7 @@ class CurveCPClientEndpoint(object):
         transport = CurveCPClientTransport(
             self.reactor, self.serverKey, fac, self.host, self.port,
             self.serverExtension, self.clientKey, self.clientExtension)
-        print self.reactor.listenUDP(0, transport)
+        self.reactor.listenUDP(0, transport)
         return transport.deferred
 
 
