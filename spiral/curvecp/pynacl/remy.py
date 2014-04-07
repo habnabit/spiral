8a9e1065dbccf480e3e86f7c545fd5c478475059
Aaron Gallagher <_@habnab.it>
2014-04-06 18:00:31 -0700

    Update Remy integration.

--- spiral/curvecp/pynacl/remy.py
+++ spiral/curvecp/pynacl/remy.py
@@ -1,48 +1,58 @@
+from __future__ import division
+
+import os
+
 from spiral.curvecp.pynacl._dna_pb2 import WhiskerTree, Memory
 
 
+ALPHA = 1 / 8
+SLOW_ALPHA = 1 / 256
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+
 class Remy(object):
-    def __init__(self, dnaString):
+    def __init__(self):
         self.tree = WhiskerTree()
-        self.tree.ParseFromString(dnaString)
-        self.rttAverage = None
-        self.deltaAverage = 0
+        with open(os.path.join(HERE, 'remy.out.0')) as infile:
+            self.tree.ParseFromString(infile.read())
         self.lastReceivedAt = None
         self.rttMin = None
+        self.recEWMA = 0
+        self.slowRecEWMA = 0
         self.window = 0
         self.secPerMessage = 0
         self.lastSentAt = 0
         self.rttTimeout = 10
 
     def processDelta(self, now, rtt):
-        if self.rttAverage is None:
-            self.rttMin = self.rttAverage = rtt
+        if self.rttMin is None:
+            self.rttMin = self.rttEWMA = rtt
         else:
-            self.rttAverage = (rtt * .125) + (self.rttAverage * .875)
             delta = now - self.lastReceivedAt
-            self.deltaAverage = (delta * .125) + (self.deltaAverage * .875)
+            self.recEWMA = (1 - ALPHA) * self.recEWMA + ALPHA * delta
+            self.slowRecEWMA = (1 - SLOW_ALPHA) * self.slowRecEWMA + SLOW_ALPHA * delta
             self.rttMin = min(rtt, self.rttMin)
         ratio = rtt / self.rttMin if self.rttMin else 1
-        print rtt, self.rttMin, ratio
-        whisker = self._findWhisker(self.deltaAverage, 0, ratio)
-        print whisker
+        whisker = self._findWhisker(self.recEWMA, self.slowRecEWMA, ratio)
         self.window = whisker.window_increment + self.window * whisker.window_multiple
-        self.secPerMessage = whisker.intersend
+        self.secPerMessage = whisker.intersend / 1000
         self.rttTimeout = self.secPerMessage * self.window
         self.lastReceivedAt = now
 
     def writerow(self, now, fobj):
         import csv
         csv.writer(fobj).writerow(
-            (now, self.rttAverage, self.rttMin, self.window, self.secPerMessage, self.rttTimeout))
+            (now, self.recEWMA, self.slowRecEWMA, self.rttMin, self.window, self.secPerMessage, self.rttTimeout))
 
     def _memoryContainedBy(self, m, mr):
         return (mr.lower.rec_rec_ewma <= m.rec_rec_ewma <= mr.upper.rec_rec_ewma
-                and mr.lower.rec_send_ewma <= m.rec_send_ewma <= mr.upper.rec_send_ewma
+                and mr.lower.slow_rec_rec_ewma <= m.slow_rec_rec_ewma <= mr.upper.slow_rec_rec_ewma
                 and mr.lower.rtt_ratio <= m.rtt_ratio <= mr.upper.rtt_ratio)
 
-    def _findWhisker(self, ackAverage, sendAverage, rttRatio):
-        memory = Memory(rec_rec_ewma=ackAverage, rec_send_ewma=sendAverage, rtt_ratio=rttRatio)
+    def _findWhisker(self, recEWMA, slowRecEWMA, rttRatio):
+        memory = Memory(
+            rec_rec_ewma=recEWMA, slow_rec_rec_ewma=slowRecEWMA,
+            rec_send_ewma=0, rtt_ratio=rttRatio)
         node = self.tree
         while True:
             if node.HasField('leaf') and self._memoryContainedBy(memory, node.leaf.domain):
