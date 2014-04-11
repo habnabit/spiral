b1c59d69c4295c6f1a777cbba4961a9a43a97cb0
Aaron Gallagher <_@habnab.it>
2014-04-11 15:36:20 -0400

    Fake cffi for readthedocs. Thanks @dstufft!

--- docs/conf.py
+++ docs/conf.py
@@ -256,3 +256,29 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+
+import sys
+
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        else:
+            return Mock()
+
+    def __floordiv__(self, other):
+        return Mock()
+
+MOCK_MODULES = [
+    "cffi", "cffi.vengine_cpy", "cffi.vengine_gen", "cffi.verifier",
+]
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
